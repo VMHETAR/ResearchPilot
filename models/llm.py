2a885e4c8b1e705047ce783bb02c62580d54abca
VMHETAR <mhetarvarad@gmail.com>
2026-06-27 07:56:32 +0530

    Updating llm.py

--- models/llm.py
+++ models/llm.py
@@ -1,6 +1,13 @@
 from abc import ABC, abstractmethod
 
+
 class BaseLLM(ABC):
+
     @abstractmethod
-    async def generate(self, system_prompt:str, user_prompt:str) -> str:
+    async def generate(
+        self,
+        system_prompt: str,
+        user_prompt: str,
+        temperature: float = 0.2,
+    ) -> str:
         pass
