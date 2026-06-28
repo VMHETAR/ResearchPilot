c0fab440b441fd0207f2a8eaa64181ddbd33fea3
VMHETAR <mhetarvarad@gmail.com>
2026-06-28 18:31:11 +0530

    Updating...

--- dataclass/search_dataclass.py
+++ dataclass/search_dataclass.py
@@ -2,11 +2,16 @@ from dataclasses import dataclass
 
 @dataclass
 class Paper:
-    title:str
-    authors:list[str]
-    abstract:str
-    year:int
-    source:str
-    url:str
-    pdf_url:str | None = None
-    doi:str | None = None
+    title: str
+    authors: list[str]
+    abstract: str
+    year: int
+    source: str
+    url: str
+
+    pdf_url: str | None = None
+    doi: str | None = None
+
+    citation_count: int = 0
+    venue: str | None = None
+    paper_id: str | None = None
