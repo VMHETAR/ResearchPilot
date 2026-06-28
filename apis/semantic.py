924c6bf917a9524216e81d7a82cc618f52716aee
VMHETAR <mhetarvarad@gmail.com>
2026-06-28 18:40:51 +0530

    Updating semantic.py

--- apis/semantic.py
+++ apis/semantic.py
@@ -1,3 +1,21 @@
 from schemas.search_dataclass import Paper
 
+async def semantic_search(query: str) -> list[Paper]:
+    # Call API
 
+    papers = []
+
+    for item in response["data"]:
+        papers.append(
+            Paper(
+                title=item["title"],
+                authors=[a["name"] for a in item["authors"]],
+                abstract=item.get("abstract", ""),
+                year=item["year"],
+                source="Semantic Scholar",
+                url=item["url"],
+                doi=item.get("externalIds", {}).get("DOI")
+            )
+        )
+
+    return papers
