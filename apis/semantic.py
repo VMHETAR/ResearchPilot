184736c325b8cd2788bc929d151fc920d83b8dd6
VMHETAR <mhetarvarad@gmail.com>
2026-06-30 14:43:54 +0530

    Commiting to semantic.py

--- apis/semantic.py
+++ apis/semantic.py
@@ -1,8 +1,11 @@
 from schemas.search_dataclass import Paper
+import requests
 
 async def semantic_search(query: str) -> list[Paper]:
 
-
+    response = requests.get(
+        url=f"https://api.semanticscholar.org/graph/v1/paper/search?q={query}&sort=relevance"
+    ).json()
     papers = []
 
     for item in response["data"]:
