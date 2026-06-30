ba25e0ffd996610cdd5c41a5782c41a5c03c1221
VMHETAR <mhetarvarad@gmail.com>
2026-06-30 14:42:25 +0530

    Commiting in arxiv.py

--- apis/arxiv.py
+++ apis/arxiv.py
@@ -1,8 +1,12 @@
-from dataclass.search_dataclass import Paper
+import feedparser
+import requests
+from schemas.search_dataclass import Paper
 
 async def arxiv_search(query: str) -> list[Paper]:
     papers = []
 
+    responses = requests.get(url=f"https://export.arxiv.org/api/query?search_query={query}&start=0&max_results=10")
+    feed = feedparser.parse(responses.text)
     for entry in feed.entries:
         papers.append(
             Paper(
@@ -12,7 +16,7 @@ async def arxiv_search(query: str) -> list[Paper]:
                 year=entry.published_parsed.tm_year,
                 source="arXiv",
                 url=entry.link,
-                pdf_url=...
+                pdf_url=entry.links[1].href
             )
         )
 
